150a7a4e6f514d65a18589cbaacc107cff4e8cd5
rabbani6020 <rabbani.cse.69@gmail.com>
2021-02-13 01:32:49 +0600

    Complete UI Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,149 +1,120 @@
-$(document).ready(function () {
-   /*==============================
-   // header
-   ================================*/
-   // if ($(window).width() <= 991) {
-   //    $(".toggle-menu").on("click", function () {
-   //       $(".header").toggleClass("menu-show");
-   //    });
-   //    $(".header-overlay, .mobile-top-cancel").on("click", function () {
-   //       $(".header").removeClass("menu-show");
-   //    });
-   //    $(".menu .menu-item .menu-link").on("click", function () {
-   //       $(this).parent("li").find(".sub-menu").slideToggle();
-   //       $(this).find("i").toggleClass("fa-chevron-down fa-chevron-up");
-   //    });
-   // }
-   /*==============================
-   // banner slider
-   ================================*/
-   // $(".banner-slider").owlCarousel({
-   //    items: 1,
-   //    loop: true,
-   //    dots: false,
-   //    nav: true,
-   //    navText: [
-   //       '<i class="fas fa-chevron-left"></i>',
-   //       '<i class="fas fa-chevron-right"></i>',
-   //    ],
-   //    autoplay: true,
-   //    autoplayTimeout: 4000,
-   //    autoplayHoverPause: true,
-   // });
-   /*=================================
-   // counter up
-   ==================================*/
-   // var counter = true;
-   // $(".counterup-area").appear();
-   // $(".counterup-area").on("appear", function () {
-   //    if (counter) {
-   //       $(".counterUp").each(function () {
-   //          var $this = $(this);
-   //          jQuery({
-   //             Counter: 0,
-   //          }).animate(
-   //             {
-   //                Counter: $this.attr("data-counter"),
-   //             },
-   //             {
-   //                duration: 3000,
-   //                easing: "swing",
-   //                step: function () {
-   //                   var num = Math.ceil(this.Counter).toString();
-   //                   if (Number(num) > 99999) {
-   //                      while (/(\d+)(\d{3})/.test(num)) {
-   //                         num = num.replace(/(\d+)(\d{3})/, "");
-   //                      }
-   //                   }
-   //                   $this.html(num);
-   //                },
-   //             }
-   //          );
-   //       });
-   //       counter = false;
-   //    }
-   // });
-   /*=============================
-   // team-slider 
-   ==============================*/
-   // $(".team-slider").owlCarousel({
-   //    items: 3,
-   //    loop: true,
-   //    margin: 30,
-   //    autoplay: true,
-   //    autoplayTimeout: 4000,
-   //    autoplayHoverPause: true,
-   //    dots: false,
-   //    nav: true,
-   //    navText: [
-   //       '<i class="fas fa-chevron-left"></i>',
-   //       '<i class="fas fa-chevron-right"></i>',
-   //    ],
-   //    responsiveClass: true,
-   //    responsive: {
-   //       0: {
-   //          items: 1,
-   //          nav: true,
-   //          margin: 0,
-   //       },
-   //       600: {
-   //          items: 2,
-   //          nav: true,
-   //       },
-   //       992: {
-   //          items: 3,
-   //          nav: true,
-   //       },
-   //    },
-   // });
-   /*=============================
-   // video pop up
-   ===============================*/
-   // $(".play-btn").magnificPopup({
-   //    type: "iframe",
-   //    iframe: {
-   //       markup:
-   //          '<div class="mfp-iframe-scaler">' +
-   //          '<div class="mfp-close"></div>' +
-   //          '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-   //          "</div>",
-   //       patterns: {
-   //          youtube: {
-   //             index: "youtube.com/",
-   //             id: "v=",
-   //             src: "https://www.youtube.com/embed/%id%?autoplay=1",
-   //          },
-   //          vimeo: {
-   //             index: "vimeo.com/",
-   //             id: "/",
-   //             src: "//player.vimeo.com/video/%id%?autoplay=1",
-   //          },
-   //          gmaps: {
-   //             index: "//maps.google.",
-   //             src: "%id%&output=embed",
-   //          },
-   //       },
-   //       srcAction: "iframe_src",
-   //    },
-   // });
-   /*==============================
-   //  Back to Top
-   ===============================*/
-   // var $window = $(window);
-   // var distance = 300;
-   // $window.scroll(function () {
-   //    if ($window.scrollTop() >= distance) {
-   //       $(".back-to-top").fadeIn();
-   //    } else {
-   //       $(".back-to-top").fadeOut();
-   //    }
-   // });
-   // $(".back-to-top").click(function () {
-   //    $("html, body").animate(
-   //       {
-   //          scrollTop: 0,
-   //       },
-   //       800
-   //    );
-   // });
-});
+const allQuestions = [
+   {
+      question: `Q1: Inside which HTML element do we put the JavaScript?`,
+      options: {
+         a: `<script>`,
+         b: `<head>`,
+         c: `<meta>`,
+         d: `<style>`,
+      },
+      corrAns: `a`,
+   },
+   {
+      question: `Q2 :- What is the correct syntax for referring to an external script called "LFC.js"?`,
+      options: {
+         a: `<script src="LFC.js">`,
+         b: `<script source="LFC.js">`,
+         c: `<script ref="LFC.js">`,
+         d: `<script type="LFC.js">`,
+      },
+      corrAns: `a`,
+   },
+   {
+      question: `Q3 :- What is the original name of JavaScript?`,
+      options: {
+         a: `LiveScript`,
+         b: `EScript`,
+         c: `Mocha`,
+         d: `JavaScript`,
+      },
+      corrAns: `c`,
+   },
+   {
+      question: `Q4 :- Which of them is not the looping structures in JavaScript?`,
+      options: {
+         a: `for`,
+         b: `while`,
+         c: `forwhich`,
+         d: `dowhile`,
+      },
+      corrAns: `c`,
+   },
+   {
+      question: `Q5 :- The "function" and " var" are known as:`,
+      options: {
+         a: `Keywords`,
+         b: `Data types`,
+         c: `Declaration statements`,
+         d: `Prototypes`,
+      },
+      corrAns: `c`,
+   },
+   {
+      question: `Q6 :- Which one of the following is the correct way for calling the JavaScript code?`,
+      options: {
+         a: `Preprocessor`,
+         b: `Triggering Event`,
+         c: `RMI`,
+         d: `Function/Method`,
+      },
+      corrAns: `d`,
+   },
+   {
+      question: `Q7 :- Which one of the following is an ternary operator:`,
+      options: {
+         a: `?`,
+         b: `: `,
+         c: `-`,
+         d: `+`,
+      },
+      corrAns: `a`,
+   },
+   {
+      question: `Q8 :- Which one of the following is an nullish coalescing operator:`,
+      options: {
+         a: `*`,
+         b: `? `,
+         c: `-`,
+         d: `??`,
+      },
+      corrAns: `d`,
+   },
+];
+
+let questionTitle = document.querySelector(".question__title");
+let option1 = document.querySelector("#option1");
+let option2 = document.querySelector("#option2");
+let option3 = document.querySelector("#option3");
+let option4 = document.querySelector("#option4");
+
+let questionWrap = document.querySelector("#question-wrap");
+
+// allQuestions.map((item, index) => {
+//    questionTitle.innerText = item.question;
+//    option1.innerText = item.options.a;
+//    option2.innerText = item.options.b;
+//    option3.innerText = item.options.c;
+//    option4.innerText = item.options.d;
+// });
+
+let questionCount = 0;
+
+const setQuestion = () => {
+   const questionList = allQuestions[questionCount];
+   questionTitle.innerText = questionList.question;
+   option1.innerText = questionList.options.a;
+   option2.innerText = questionList.options.b;
+   option3.innerText = questionList.options.c;
+   option4.innerText = questionList.options.d;
+};
+
+setQuestion();
+
+// for(; questionCount <= allQuestions.length; questionCount++){
+//    const questionList = allQuestions[questionCount];
+//    questionTitle.innerText = questionList.question;
+//    option1.innerText = questionList.options.a;
+//    option2.innerText = questionList.options.b;
+//    option3.innerText = questionList.options.c;
+//    option4.innerText = questionList.options.d;
+// }
---
+++ assets/js/quiz-data.js
@@ -0,0 +1,82 @@
+const questions = [
+   {
+      question: `Inside which HTML element do we put the JavaScript?`,
+      options: {
+         a: `<script>`,
+         b: `<head>`,
+         c: `<meta>`,
+         d: `<style>`,
+      },
+      corrAns: `a`,
+   },
+   {
+      question: `What is the correct syntax for referring to an external script called "LFC.js"?`,
+      options: {
+         a: `<script src="LFC.js">`,
+         b: `<script source="LFC.js">`,
+         c: `<script ref="LFC.js">`,
+         d: `<script type="LFC.js">`,
+      },
+      corrAns: `a`,
+   },
+   {
+      question: `What is the original name of JavaScript?`,
+      options: {
+         a: `LiveScript`,
+         b: `EScript`,
+         c: `Mocha`,
+         d: `JavaScript`,
+      },
+      corrAns: `c`,
+   },
+   {
+      question: `Which of them is not the looping structures in JavaScript?`,
+      options: {
+         a: `for`,
+         b: `while`,
+         c: `forwhich`,
+         d: `dowhile`,
+      },
+      corrAns: `c`,
+   },
+   {
+      question: `The "function" and " var" are known as:`,
+      options: {
+         a: `Keywords`,
+         b: `Data types`,
+         c: `Declaration statements`,
+         d: `Prototypes`,
+      },
+      corrAns: `c`,
+   },
+   {
+      question: `Which one of the following is the correct way for calling the JavaScript code?`,
+      options: {
+         a: `Preprocessor`,
+         b: `Triggering Event`,
+         c: `RMI`,
+         d: `Function/Method`,
+      },
+      corrAns: `d`,
+   },
+   {
+      question: `Which one of the following is an ternary operator:`,
+      options: {
+         a: `?`,
+         b: `: `,
+         c: `-`,
+         d: `+`,
+      },
+      corrAns: `a`,
+   },
+   {
+      question: `Which one of the following is an nullish coalescing operator:`,
+      options: {
+         a: `?`,
+         b: `: `,
+         c: `-`,
+         d: `??`,
+      },
+      corrAns: `d`,
+   },
+];
